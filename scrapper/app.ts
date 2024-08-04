ac41c02cf546032974fd5189421a907353db9757
Raul Garcia <raulfgarcia10@gmail.com>
2024-08-04 19:34:22 -0300

    feat: creates lambdaHandler

--- scrapper/app.ts
+++ scrapper/app.ts
@@ -1,30 +1,47 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { SQSEvent } from 'aws-lambda';
+import { z, ZodError } from 'zod';
+import { AxiosHttpGateway } from './adapters/axios-http-gateway';
+import { CheerioNfeParser } from './adapters/cheerio-nfe-parser';
+import { SnsMessageBroker } from './adapters/sns-message-broker';
+import { HttpGateway } from './bondaries/http-gateway';
+import { NfeParser } from './bondaries/nfe-parser';
+import { MessageBroker } from './bondaries/message-broker';
+import { ExtractAndPublishNfeUseCase } from './use-cases/extract-and-publish-nfe-data-use-case';
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+export async function lambdaHandler(event: SQSEvent): Promise<void> {
+    const recordSchema = z.array(
+        z.object({
+            url: z.string().url(),
+        }),
+    );
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
+        const records = recordSchema.parse(
+            event.Records.map((record) => ({
+                url: JSON.parse(record.body).url,
+            })),
+        );
+
+        const httpGateway: HttpGateway = new AxiosHttpGateway();
+        const nfeParser: NfeParser = new CheerioNfeParser();
+        const messageBroker: MessageBroker = new SnsMessageBroker();
+
+        const extractAndPublishNfeDataUseCase = new ExtractAndPublishNfeUseCase(httpGateway, nfeParser, messageBroker);
+
+        for (const record of records) {
+            await extractAndPublishNfeDataUseCase.execute({ url: record.url });
+        }
+        return;
+    } catch (error: unknown) {
+        if (error instanceof ZodError) {
+            console.log(error.format());
+            return;
+        }
+        if (error instanceof Error) {
+            console.log(error.message);
+            return;
+        }
+        console.log(error);
+        return;
     }
-};
+}
